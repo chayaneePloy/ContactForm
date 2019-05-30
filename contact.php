bdfdb19f977383da8a2444936127a34810b4594c
chayaneePloy <chayanee4039@gmail.com>
2019-05-30 14:04:29 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'website@clearvision-properties.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'fredrik@clearvision-properties.com';
+$sendTo = 'ploy@clearvision-properties.com';
  
 // subject of the email
 //$subject = 'New message from contact form';
